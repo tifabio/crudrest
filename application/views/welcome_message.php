0c095d786e1b097347517baab705c3b955785a2e
tifabio <tifabio@gmail.com>
2019-03-07 11:43:45 -0300

    Update welcome_message.php

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -4,24 +4,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 <html lang="en">
 <head>
 	<meta charset="utf-8">
-	<title>CRUD REST SWAGGER</title>
-
-	<style type="text/css">
-		* {
-			padding: 0;
-			margin: 0;
-			width: 100%;
-			min-height: 99.8%;
-    		height: 99.8%;
-			border: 0;
-		}
-		iframe {
-			min-height: 100%;
-    		height: 100%;
-		}
-	</style>
+	<metra http-equiv="refresh" content="1;URL=https://restcrud.herokuapp.com/public"> 
 </head>
 <body>
-  <iframe src="https://swaggerhub.com/apis/tifabio/crud-rest/0.0.1"></iframe>
 </body>
 </html>
